e740905eda93973ed15dab90a23e57a1ffc2dc4b
jacmat22 <jacopo.m@live.it>
2020-11-09 18:07:22 +0100

    Modelli e Service per schedaVotazione e UtenteVotante

---
+++ src/main/java/it/contrader/model/UtenteVotante.java
@@ -0,0 +1,24 @@
+package it.contrader.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class UtenteVotante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_utente;
+	private int id_scheda;
+	private int voto;
+}
---
+++ src/main/java/it/contrader/service/UtenteVotanteService.java
@@ -0,0 +1,33 @@
+package it.contrader.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import it.contrader.converter.UtenteVotanteConverter;
+import it.contrader.dao.UtenteVotanteRepository;
+import it.contrader.dto.UtenteVotanteDTO;
+import it.contrader.model.UtenteVotante;
+
+@Service
+public class UtenteVotanteService extends AbstractService<UtenteVotante, UtenteVotanteDTO> {
+
+	@Autowired
+	private UtenteVotanteConverter converter;
+	@Autowired
+	private UtenteVotanteRepository repository;
+
+	public boolean checkUser(int id_scheda, int id_utente) {
+		return converter.checkUser(repository.findByIdutenteAndIdscheda(id_utente, id_scheda));
+		}
+	
+	public double[] getStatistica(int id_scheda) {
+		double[] ar = new double[3];
+		ar[0] = (double)converter.getStatistica(repository.countVoto(id_scheda, 1));
+		ar[1] = (double)converter.getStatistica(repository.countVoto(id_scheda, 2));
+		ar[2] = (double)converter.getStatistica(repository.countVoto(id_scheda, 3));
+		return  ar;
+
+	}
+
+}
