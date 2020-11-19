f57a2976e3e940623acaac9c8270d9640a974b65
Matteo Sbicego <matteo.sbicego96@gmail.com>
2020-11-19 12:59:06 +0100

    aggiunto componente lista votanti

--- src/app/admin/votanti-lista/votanti-lista.component.ts
+++ src/app/admin/votanti-lista/votanti-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { utentevotanteDTO } from '../../../dto/utentevotantedto';
+import { UtenteVotanteService } from '../../../service/utente-votante.service';
 
 @Component({
   selector: 'app-votanti-lista',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotantiListaComponent implements OnInit {
 
-  constructor() { }
+  listaUserV: utentevotanteDTO[];
+  constructor(
+    private service: UtenteVotanteService
+  ) { }
 
   ngOnInit() {
+    this.getList();
+  }
+
+  getList(){
+    this.service.getAll().subscribe(listaUserV => {this.listaUserV = listaUserV},undefined,null);
   }
 
 }
